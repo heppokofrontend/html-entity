e4be355a2655fa3c88eca00a6cdf9debf318bf2b
Hiroya🍣🍵 <heppokofrontend@gmail.com>
2021-03-27 22:13:36 +0900

    :rotating_light: Update

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,3 +4,17 @@ test('Basic usege', () => {
   expect(encodeHTMLEntities('hogehoge')).toBe('&#x68;&#x6f;&#x67;&#x65;&#x68;&#x6f;&#x67;&#x65;');
   expect(decodeHTMLEntities('&#x68;&#x6f;&#x67;&#x65;&#x68;&#x6f;&#x67;&#x65;')).toBe('hogehoge');
 });
+
+const testCases = [
+  'こんにちは',
+  '😇',
+];
+
+for (const base of testCases) {
+  const entity = encodeHTMLEntities(base);
+
+  test(base, () => {
+    expect(encodeHTMLEntities(base)).toBe(entity);
+    expect(decodeHTMLEntities(entity)).toBe(base);
+  });
+}
